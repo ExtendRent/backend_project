f61cb4dd4fa111b301ca8b51c3090a1af60115c8
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2023-12-08 17:52:18 +0300

    project created

--- src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
+++ src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
@@ -9,5 +9,5 @@ public class TobetoRentAcarCrewProjectApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TobetoRentAcarCrewProjectApplication.class, args);
 	}
-
+//sdafds
 }
