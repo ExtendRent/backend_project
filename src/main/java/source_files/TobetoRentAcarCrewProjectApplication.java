be2718a30babf513731f3b3cd84166c5bd62cc61
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2023-12-08 17:35:55 +0300

    project created

---
+++ src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
@@ -0,0 +1,13 @@
+package source_files;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobetoRentAcarCrewProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TobetoRentAcarCrewProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/source_files/TobetoRentAcarCrewProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package source_files;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TobetoRentAcarCrewProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
