33bc4f847a6e7171d83e2e3a204236cd89a472de
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2023-12-09 23:46:21 +0300

    deneme

--- src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
+++ src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
@@ -8,6 +8,8 @@ public class TobetoRentAcarCrewProjectApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TobetoRentAcarCrewProjectApplication.class, args);
+
+        System.out.println("deneme");
     }
 
 }
