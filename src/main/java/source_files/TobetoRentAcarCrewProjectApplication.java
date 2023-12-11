28680e6d9b953bc602c44cddbb9975b23a1cb9db
Rukiye Arabacı <rukiye.arbc6@gmail.com>
2023-12-11 16:42:32 +0300

    deneme

--- src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
+++ src/main/java/source_files/TobetoRentAcarCrewProjectApplication.java
@@ -2,8 +2,9 @@ package source_files;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
 public class TobetoRentAcarCrewProjectApplication {
 
 	public static void main(String[] args) {
