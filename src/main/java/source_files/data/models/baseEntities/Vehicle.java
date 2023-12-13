35abfa287043a293be3a6f5b4507912234f05487
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2023-12-13 16:43:51 +0300

    refactored

--- src/main/java/source_files/data/models/baseEntities/Vehicle.java
+++ src/main/java/source_files/data/models/baseEntities/Vehicle.java
@@ -7,7 +7,6 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.experimental.SuperBuilder;
-import source_files.data.models.vehicleEntities.vehicleFeatures.CarFeatures.BrandEntity;
 import source_files.data.models.vehicleEntities.vehicleFeatures.CarFeatures.ColorEntity;
 import source_files.data.types.DrivingLicenseType;
 
@@ -21,10 +20,6 @@ import java.util.List;
 @MappedSuperclass //Alt klasların database tablosuna buradaki kolonları eklemek için kullanılır.
 @Inheritance(strategy = InheritanceType.JOINED) // kendini extend eden her klasa kendi değişkenlerini eklemesini sağlar.
 public class Vehicle extends BaseEntity {
-
-    @ManyToOne
-    @JoinColumn(name = "brand_id")
-    private BrandEntity brandEntity;
 
     @ManyToOne
     @JoinColumn(name = "color_id")
