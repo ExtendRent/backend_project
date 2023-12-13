1b2803d480091c95bcf55ed9a2aa63c5e1c4b8d8
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2023-12-13 15:11:11 +0300

    error fixed - mapstruct import removed

--- src/main/java/source_files/data/DTO/Mappers/VehicleMappers/CarMapper.java
+++ src/main/java/source_files/data/DTO/Mappers/VehicleMappers/CarMapper.java
@@ -1,10 +1,10 @@
 package source_files.data.DTO.Mappers.VehicleMappers;
 
-import org.mapstruct.Mapper;
+
 import source_files.data.DTO.vehicleDTOs.CarDTO;
 import source_files.data.models.vehicleEntities.CarEntity;
 
-@Mapper
+
 public interface CarMapper {
     CarDTO carEntityToCarDTO(CarEntity carEntity);
 }
