2cab666510ffcc74a9c18da3bb507247585a6b27
Gökhan Asiltürk <gokhanasilturkk@gmail.com>
2024-01-21 14:58:55 +0300

    createUser method added.

--- src/main/java/source_files/services/userServices/UserManager.java
+++ src/main/java/source_files/services/userServices/UserManager.java
@@ -1,14 +1,15 @@
 package source_files.services.userServices;
 
-import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import source_files.data.models.baseEntities.UserEntity;
 import source_files.dataAccess.userRepositories.UserRepository;
 import source_files.services.userServices.abstracts.UserService;
 
 @Service
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class UserManager implements UserService {
 
     private final UserRepository userRepository;
@@ -17,5 +18,10 @@ public class UserManager implements UserService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepository.findByEmailAddress(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found!"));
+    }
+
+    @Override
+    public void createUser(UserEntity user) {
+        userRepository.save(user);
     }
 }
--- src/main/java/source_files/services/userServices/abstracts/UserService.java
+++ src/main/java/source_files/services/userServices/abstracts/UserService.java
@@ -1,6 +1,8 @@
 package source_files.services.userServices.abstracts;
 
 import org.springframework.security.core.userdetails.UserDetailsService;
+import source_files.data.models.baseEntities.UserEntity;
 
 public interface UserService extends UserDetailsService {
+    void createUser(UserEntity user);
 }
